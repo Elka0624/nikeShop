bf83147f6c03a6a25c5652b5279f8bd4f5d41ec9
Elyor <ilhomovelyor20010624@gmail.com>
2023-04-26 20:11:45 +0500

    some change

--- nike/src/App.js
+++ nike/src/App.js
@@ -1,14 +1,16 @@
 import './App.css'
-import Carousel from './Companents/Carousel/Carousel';
+import DemoCarousel from './Companents/Carousel/Carousel';
 import Navbar from './Companents/Navbar/Navbar';
 import NavInfo from './Companents/NavInfo/NavInfo';
+import TopMain from './Companents/TopMain/TopMain';
 
 function App() {
   return (
     <div className="App">
       <NavInfo />
       <Navbar />
-      <Carousel />
+      <DemoCarousel />
+      <TopMain />
     </div>
   );
 }
---
+++ nike/src/Companents/TopMain/TopMain.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import {TbTruckDelivery} from 'react-icons/tb'
+
+
+const TopMain = () => {
+return (
+    <div>
+        <div className="container">
+            <div className="topMain flex">
+                <div className="divgreen">
+                    <TbTruckDelivery className='icon'/>
+                    <p>hllo</p>
+                </div>
+                <div className="divgreen">
+                    <img src="" alt="" />
+                    <p></p>
+                </div>
+                <div className="divblue">
+                    <img src="" alt="" />
+                    <p></p>
+                </div>
+                <div className="divblue">
+                    <img src="" alt="" />
+                    <p></p>
+                </div>
+            </div>
+        </div>
+    </div>
+)
+}
+
+export default TopMain
