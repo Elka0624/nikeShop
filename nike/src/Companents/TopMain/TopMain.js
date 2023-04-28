0981d51db824437b2a892d7495d247f5021f452f
Elyor <ilhomovelyor20010624@gmail.com>
2023-04-28 20:29:49 +0500

    some change

--- nike/src/Companents/TopMain/TopMain.js
+++ nike/src/Companents/TopMain/TopMain.js
@@ -1,29 +1,89 @@
 import React from 'react'
-import {TbTruckDelivery} from 'react-icons/tb'
+import {TbTruckDelivery} from 'react-icons/tb';
+import {AiOutlinePercentage} from 'react-icons/ai'
+import {MdOutlineDeliveryDining, MdOutlineContactPhone} from 'react-icons/md';
+import {BrowserRouter, Route, Link} from 'react-router-dom';
+import {GiRunningShoe, GiTShirt} from 'react-icons/gi';
+import {FaFemale} from 'react-icons/fa';
+import {RxDashboard} from 'react-icons/rx'
+import './TopMain.css'
 
 
 const TopMain = () => {
 return (
     <div>
         <div className="container">
-            <div className="topMain flex">
-                <div className="divgreen">
-                    <TbTruckDelivery className='icon'/>
-                    <p>hllo</p>
+            <BrowserRouter>
+                <div className="topMain flex">
+                    <div className="divgreen">
+                        <Link to='#' className='topLink'>
+                            <div className="icondiv">
+                                <TbTruckDelivery className='icon'/>
+                            </div>
+                            <p className='linkParagraph'>Tezkor Yetkazish</p>
+                        </Link>
+                    </div>
+                    <div className="divgreen">
+                        <Link to='#' className='topLink'>
+                            <div className="icondiv">
+                                    <AiOutlinePercentage className='icon'/>
+                            </div>
+                            <p className='linkParagraph'>Aksiyalar 🔥</p>
+                        </Link>
+                    </div>
+                    <div className="divblue">
+                        <Link to='#' className='topLink'>
+                            <div className="icondiv">
+                                <MdOutlineDeliveryDining className='icon'/>
+                            </div>
+                            <p className='linkParagraph'>Bepul Yetkazish</p>
+                        </Link>
+                    </div>
+                    <div className="divblue">
+                        <Link to='#' className='topLink'>
+                            <div className="icondiv">
+                                <MdOutlineContactPhone className='icon'/>
+                            </div>
+                            <p className='linkParagraph'>Aloqa Uchun</p>
+                        </Link>
+                    </div>
                 </div>
-                <div className="divgreen">
-                    <img src="" alt="" />
-                    <p></p>
+                {/* ikkinchi qator boshlandi */}
+                <div className="topMain flex">
+                    <div className="divblue">
+                            <Link to='#' className='topLink'>
+                                <div className="icondiv">
+                                    <GiRunningShoe className='icon'/>
+                                </div>
+                                <p className='linkParagraph'>Krasofkalar</p>
+                            </Link>
+                    </div>
+                    <div className="divblue">
+                            <Link to='#' className='topLink'>
+                                <div className="icondiv">
+                                    <GiTShirt className='icon'/>
+                                </div>
+                                <p className='linkParagraph'>Fudbolkalar</p>
+                            </Link>
+                    </div>
+                    <div className="divgreen">
+                        <Link to='#' className='topLink'>
+                            <div className="icondiv">
+                                <FaFemale className='icon'/>
+                            </div>
+                            <p className='linkParagraph'>Ayollar Uchun</p>
+                        </Link>
+                    </div>
+                    <div className="divgreen">
+                        <Link to='#' className='topLink'>
+                            <div className="icondiv">
+                                <RxDashboard className='iconn'/>
+                            </div>
+                            <p className='linkParagraph'>Barchasi</p>
+                        </Link>
+                    </div>
                 </div>
-                <div className="divblue">
-                    <img src="" alt="" />
-                    <p></p>
-                </div>
-                <div className="divblue">
-                    <img src="" alt="" />
-                    <p></p>
-                </div>
-            </div>
+            </BrowserRouter>
         </div>
     </div>
 )
---
+++ nike/src/Companents/UpdateLater/UpdateLater.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const UpdateLater = () => {
+return (
+    <div>
+        
+    </div>
+)
+}
+
+export default UpdateLater
